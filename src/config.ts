e98827f817f9b926943c5e618cb0af5ff34baed4
arvin-bit <120641674+arvin-bit@users.noreply.github.com>
2025-10-01 11:38:04 +0200

    Starter shown

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export const siteConfig = {
     {
       name: "KaPlay starter",
       description:
-        "A starter kit using KaPlay for schools.",
+        "A starter kit using KaPlay for no reason except showing here.",
       link: "https://github.com/arvin-bit/kaplaystarter",
       skills: ["TypeScript", "Node-js"],
     }
